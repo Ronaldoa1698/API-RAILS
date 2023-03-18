b80dfa063225ef73a93042be29ad6e2f310d8525
Ronaldoa1698 <ronaldo.delgado@unas.edu.pe>
2023-03-18 09:53:10 -0500

    generate the spec file api/v1/users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,8 +5,13 @@ class Api::V1::UsersController < Api::V1::BaseController
         return render json:{status: created}
       end
 
-    render json:@user.errors, status: :unprocessable_entity
+    render json: @user.errors, status: :unprocessable_entity
   end
 
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 
 end
